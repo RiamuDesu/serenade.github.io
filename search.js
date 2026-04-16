b55ca1261627668dae6cedd58b371a792993ffca
RiamuDesu <jiamflojo13@gmail.com>
2026-04-16 10:09:05 +0800

    Created Filtering, Please add better designs

---
+++ search.js
@@ -0,0 +1,22 @@
+function filterByCategory() {
+    console.log("Attempting to sort...")
+    const selection = document.getElementById('categoryFilter').value;
+
+    const boards = document.querySelectorAll('.filterable');
+
+    boards.forEach(board => {
+        if (selection === "all") {
+            board.classList.remove('hidden');
+        } else {
+            if (board.id === selection) {
+                board.classList.remove('hidden');
+            } else {
+                board.classList.add('hidden');
+            }
+        }
+    });
+}
+
+function openGallery(el) {
+    console.log("Opening gallery for", el);
+}
