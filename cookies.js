4a338794ec9c30546493c1591fff8f2cc314b413
riveraeiraayessa-lgtm <rivera.eiraayessa@ue.edu.ph>
2026-04-19 00:22:54 +0800

    eira

--- cookies.js
+++ cookies.js
@@ -1,45 +1,53 @@
-function transferData(){
+function transferData() {
     const cname = document.getElementById("name").value;
     const cpass = document.getElementById("password").value;
     registerUser(cname, cpass);
 }
-function registerUser(cname, cpass){
+
+function registerUser(cname, cpass) {
     const d = new Date();
     d.setTime(d.getTime() + (1 * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
+    let expires = "expires=" + d.toUTCString();
     document.cookie = "Username=" + cname + ";" + expires + ";path=/";
     document.cookie = "Password=" + cpass + ";" + expires + ";path=/";
+    
+    // Redirect sa index page pagka-login
+    window.location.assign("index.html");
 }
-function getUser(){
+
+function getUser() {
     let cookies = document.cookie.split(";");
     for (let i = 0; i < cookies.length; i++) {
         let c = cookies[i].trim();
         if (c.indexOf("Username=") === 0) {
-        return c.substring("Username=".length);
+            return c.substring("Username=".length);
         }
     }
     return null;
 }
-function onPageLoad(){
-    console.log("onLoad Excecuted");
+
+function onPageLoad() {
+    console.log("onLoad Executed");
     let user = getUser();
-    if(user){
-        
-        document.getElementById("welcometext").innerHTML = "Welcome " + user + "!" 
-    }
-    else{
+    if (user) {
+        // Kung may 'welcometext' na ID sa page, papalitan niya
+        let welcomeEl = document.getElementById("welcometext");
+        if (welcomeEl) {
+            welcomeEl.innerHTML = "Welcome " + user + "!";
+        }
+    } else {
+        // Pag walang nahanap na user sa cookies, sipa pabalik sa login
         window.location.assign("login.html");
     }
 }
-function logoutUser(){
-    const cookies = document.cookie.split(";");
 
+function logoutUser() {
+    const cookies = document.cookie.split(";");
     for (let i = 0; i < cookies.length; i++) {
         const cookie = cookies[i];
         const eqPos = cookie.indexOf("=");
         const name = eqPos > -1 ? cookie.substr(0, eqPos).trim() : cookie.trim();
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/";
     }
-
     window.location.assign("login.html");
 }
